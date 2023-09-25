01ef19cc1013e52ddc718d66be361b0d53d68ad2
Ioannis Loudaros <johnloudaros@hotmail.com>
2023-09-25 17:37:24 +0300

    Update lsi_demo.py

--- Contributions/iloudaros/lsi_demo.py
+++ Contributions/iloudaros/lsi_demo.py
@@ -6,8 +6,9 @@ from tkinter import messagebox
 
 def Demo():
     intersection_points_1 = pol_interesect_naive(lake.points, country1.points)
-    intersection_points_2 = pol_intersect_sweep_line(lake.points, country2.points)
+    intersection_points_2 = pol_interesect_naive(lake.points, country2.points)
 
+    print ("The intersection points are: ", intersection_points_1, intersection_points_2)
     if intersection_points_1 == intersection_points_2 : 
         print("The lake is a natural border, intersecting the two countries at coordinatess", intersection_points_2)
         tkinter.messagebox.showinfo("There is an intersection point!",  ("The lake is a natural border, intersecting the two countries at coordinatess: \n Point 1: %s \nPoint 2: %s " %(intersection_points_2[0],intersection_points_2[1])) )
